36e895549db4ee890347bd1b30f0637faed26fa8
James David Saul <jamesdavidsaul@gmail.com>
2025-07-06 10:59:00 -0400

    Add header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Header from '@/components/Header';
 import TextHighlight from '@/components/TextHighlight';
 import FlickrGallery from '@/components/FlickrGallery';
 import Footer from '@/components/Footer';
@@ -6,6 +7,7 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <main>
+      <Header />
       <TextHighlight />
       <FlickrGallery />
       {/* <Footer /> */}
